7e30aea0112530e1ca5d85ee8e1ddb2d7e3de364
Michael Selby <mikeselby2@hotmail.co.uk>
2022-05-25 12:24:30 +0100

    Tutorial

---
+++ Assets/Scripts/UI/timer.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour
+{
+    private float seconds;
+    private int min;
+    public  int hour;
+    private int day;
+    public float timeMod;
+
+    public Text TimeDisplay;
+    public Text SecDisplay;
+
+    public GameObject FailedQuota;
+
+    private void Update()
+    {
+
+        seconds += Time.deltaTime * timeMod;
+
+        if (seconds >= 60)
+        {
+            min += (int)(seconds / 60);
+            seconds %= 60;
+        }
+
+        if (min >= 60)
+        {
+            hour += min / 60;
+            min %= 60;  
+        }
+
+        /*
+        if (hour >= 24)
+        {
+            day += hour / 24;
+            hour %= 24;
+        }
+        */
+
+        TimeDisplay.text = min.ToString() + " : " + hour.ToString(); //+ " : " + day.ToString();
+    }
+}
+
