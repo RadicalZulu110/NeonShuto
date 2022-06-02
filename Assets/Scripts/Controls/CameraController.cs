15897551982e7620453884f2c84701a67236ae39
Michael Selby <mikeselby2@hotmail.co.uk>
2022-06-02 20:13:44 +0100

    Info + messing with camera in test scene

--- Assets/Scripts/Controls/CameraController.cs
+++ Assets/Scripts/Controls/CameraController.cs
@@ -77,6 +77,7 @@ public class CameraController : MonoBehaviour
         {
             rotateStartPosition = Input.mousePosition;
         }
+        
         if(Input.GetMouseButton(2))
         {
             rotateCurrentPosition = Input.mousePosition;
@@ -86,7 +87,20 @@ public class CameraController : MonoBehaviour
             rotateStartPosition = rotateCurrentPosition;
 
             newRotation *= Quaternion.Euler(Vector3.up * (-difference.x / 5f));
+            newRotation *= Quaternion.Euler(Vector3.right * (difference.y / 5f));
         }
+        /*
+        if (Input.GetMouseButton(2))
+        {
+            rotateCurrentPosition = Input.mousePosition;
+
+            Vector3 difference = rotateStartPosition - rotateCurrentPosition;
+
+            rotateStartPosition = rotateCurrentPosition;
+
+            newRotation *= Quaternion.Euler(Vector3.right * (difference.y / 5f));
+        }
+        */
     }
 
     void HandleMovmentInput()
